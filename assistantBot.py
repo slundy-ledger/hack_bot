e96f489175ec488d786f9e0e195893d56c78d469
Slundy Ledger <stephane.lundy@ledger.fr>
2023-05-11 11:59:25 +0200

    refacto

--- assistantBot.py
+++ assistantBot.py
@@ -15,19 +15,10 @@ from langchain.memory import ChatMessageHistory
 
 from web3 import Web3
 
+from data import ABI, ADDRESS, FAKE_RESPONSE, primer, browser_headers
+
 
 load_dotenv()
-
-FAKE_RESPONSE = """Ledger Live Pro is a feature available only for Ledger Live desktop and is designed specifically for traders. This version includes a more advanced interface, real-time market data, more advanced order types, and the option to connect multiple trading platforms to Ledger Live. If you're interested in using Ledger Live Pro, you'll need to have Ledger Live installed on your desktop. Here's how you can download and install Ledger Live:
-
-- First, navigate to the official Ledger website at https://www.ledger.com/ledger-live/download/.
-- Next, select the appropriate operating system, for example, Windows, macOS, or Linux, and choose the download option for 'Ledger Live'.
-- Once downloaded, open the Ledger Live executable file and follow the on-screen instructions to complete the installation process.
-- Once you have installed Ledger Live on your desktop, you can start using Ledger Live Pro. Simply open Ledger Live desktop and click on the 'Pro' button located in the top right corner of the interface.
-- Please note that to enable Ledger Live Pro, you will need to switch on the 'Advanced mode' from the application settings.
-
-For more information about Ledger Live Pro, including its features and how to use it, you can refer to the official Ledger Support website at https://support.ledger.com/hc/en-us/articles/360024436233-Ledger-Live-Pro.
-"""
 
 history = ChatMessageHistory()
 
@@ -71,39 +62,6 @@ index_name = "hc"
 index = pinecone.Index(index_name)
 
 embed_model = "text-embedding-ada-002"
-
-primer = """
-
-You are Amy, a highly intelligent and helpful virtual assistant designed to support Ledger, a French cryptocurrency company led by CEO Pascal Gauthier. Your primary responsibility is to assist Ledger customer support agents by providing accurate answers to their questions. If a question is unclear or lacks detail, ask for more information instead of making assumptions. If you are unsure of an answer, be honest and seek clarification.
-
-Customers may ask about various Ledger products, including the Ledger Nano S (no battery, low storage), Nano X (Bluetooth, large storage, has a battery), Nano S Plus (large storage, no Bluetooth, no battery), Ledger Stax (unreleased), and Ledger Live.
-The official Ledger store is located at https://shop.ledger.com/. For authorized resellers, please visit https://www.ledger.com/reseller/ , do not modify or share any other links for these purposes.
-
-When agents inquire about tokens, crypto or coins supported in Ledger Live , it is crucial to strictly use the provided Crypto Asset List link to verify support.
-
-The link to the Crypto Asset List of supported crypto coins and tokens is: https://support.ledger.com/hc/en-us/articles/10479755500573?docs=true/. Do NOT provide any other links to the list.
-
-Here's a guide on using the Crypto Asset List:
-
-- The list is organized into bullet points with the following format: | network | token symbol | contract address | token name | countervalue
-- If a coin or token is on the list, it is supported in Ledger Live.
-- The list is not case-sensitive, so "BTC" and "btc" are treated as identical.
-- If a coin or token has "countervalues disabled" as its countervalue, it is supported in Ledger Live, but its value will not be displayed.
-- When a coin or token is supported, include information about the supported network.
-- ALWAYS provide the following link to the Crypto Asset List for users to find more information:  https://support.ledger.com/hc/en-us/articles/10479755500573?docs=true/ . Do not share any other links for this purpose.
-
-
-VERY IMPORTANT:
-
-- Always mention the source of your information (URL link) when providing answers, such as an official Help Center article or tutorial. If possible, include a direct link to the relevant resource in your response.
-- Provide the correct URL link to relevant Help Center articles or tutorials when responding. Do not share a link if uncertain of its accuracy.
-- Direct users who want to learn more about Ledger products or compare devices to https://www.ledger.com/.
-- Updating or downloading Ledger Live must always be done via this link: https://www.ledger.com/ledger-live
-- Share this list for tips on keeping your recovery phrase safe: https://support.ledger.com/hc/en-us/articles/360005514233-How-to-keep-your-24-word-recovery-phrase-and-PIN-code-safe-?docs=true/
-
-Begin! Keep in mind that your primary objective is to assist them in effectively performing their duties.
-
-"""
 
 # #####################################################
 
@@ -162,38 +120,11 @@ def react_description():
     user_input = request.json.get("user_input")
 
     try:
-        res_embed = openai.Embedding.create(input=[user_input], engine=embed_model)
-
-        xq = res_embed["data"][0]["embedding"]
-
-        res_query = index.query(xq, top_k=5, include_metadata=True)
-
-        contexts = [item["metadata"]["text"] for item in res_query["matches"]]
-
-        augmented_query = (
-            "\n\n---\n\n".join(contexts)
-            + "\n\n-----\n\n"
-            + user_input
-            + "? Please provide a comprehensive answer to the question, and make sure to incorporate relevant URL links from the previous context. Do not enclose the links in parentheses. Don't share a link that is not included in the previous context. Never share links containing 'academy' in the URL. Important : format your response using markdown syntax. When writing a list, use dash instead of numbers."
-        )
-
-        print(augmented_query)
-
-        res = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            # model="gpt-4",
-            messages=[
-                {"role": "system", "content": primer},
-                {"role": "user", "content": augmented_query},
-            ],
-        )
-
         # response = FAKE_RESPONSE
-
-        response = res["choices"][0]["message"]["content"]
+        response = ask_gpt(user_input)
 
         print(response)
-        test_urls_from_response_okay(response)
+        # test_urls_from_response_okay(response)
         return {"output": response}
 
     except ValueError as e:
@@ -201,309 +132,49 @@ def react_description():
         return {"output": "Sorry, could you please repharse the question?"}
 
 
+def ask_gpt(user_input: str) -> str:
+    res_embed = openai.Embedding.create(input=[user_input], engine=embed_model)
+
+    xq = res_embed["data"][0]["embedding"]
+
+    res_query = index.query(xq, top_k=5, include_metadata=True)
+
+    contexts = [item["metadata"]["text"] for item in res_query["matches"]]
+
+    augmented_query = (
+        "\n\n---\n\n".join(contexts)
+        + "\n\n-----\n\n"
+        + user_input
+        + "? Please provide a comprehensive answer to the question, and make sure to incorporate relevant URL links from the previous context. Do not enclose the links in parentheses. Don't share a link that is not included in the previous context. Never share links containing 'academy' in the URL. Important : format your response using markdown syntax. When writing a list, use dash instead of numbers."
+    )
+
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        temperature=0.1,
+        # model="gpt-4",
+        messages=[
+            {"role": "system", "content": primer},
+            {"role": "user", "content": augmented_query},
+        ],
+    )
+
+    return res["choices"][0]["message"]["content"]
+
+
 def test_urls_from_response_okay(text: str):
-    browser_headers = {
-        "authority": "support.ledger.com",
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-        "cache-control": "max-age=0",
-        "sec-ch-ua": '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "macOS",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-origin",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-    }
     url_pattern = re.compile(r"https?://\S+")
     urls = re.findall(url_pattern, text)
     formatted_urls = [url.rstrip(".") for url in urls]
     for url in formatted_urls:
         if "academy" in url:
             raise ValueError(url)
-        """
-        res = requests.get(url, browser_headers)
+
+        res = requests.head(url, headers=browser_headers)
         if res.status_code in [401, 403, 404]:
-            # always get 403, ledger is protected from bot even with browser headers 
+            # always get 403, ledger is protected from bot even with browser headers
             raise ValueError(url)
-        """
 
 
-ADDRESS = "0xb022C9c672592c274397557556955eE968052969"
-ABI = [
-    {
-        "inputs": [
-            {"internalType": "string", "name": "_name", "type": "string"},
-            {"internalType": "string", "name": "_symbol", "type": "string"},
-        ],
-        "stateMutability": "nonpayable",
-        "type": "constructor",
-    },
-    {
-        "anonymous": False,
-        "inputs": [
-            {
-                "indexed": True,
-                "internalType": "address",
-                "name": "owner",
-                "type": "address",
-            },
-            {
-                "indexed": True,
-                "internalType": "address",
-                "name": "approved",
-                "type": "address",
-            },
-            {
-                "indexed": True,
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256",
-            },
-        ],
-        "name": "Approval",
-        "type": "event",
-    },
-    {
-        "anonymous": False,
-        "inputs": [
-            {
-                "indexed": True,
-                "internalType": "address",
-                "name": "owner",
-                "type": "address",
-            },
-            {
-                "indexed": True,
-                "internalType": "address",
-                "name": "operator",
-                "type": "address",
-            },
-            {
-                "indexed": False,
-                "internalType": "bool",
-                "name": "approved",
-                "type": "bool",
-            },
-        ],
-        "name": "ApprovalForAll",
-        "type": "event",
-    },
-    {
-        "anonymous": False,
-        "inputs": [
-            {
-                "indexed": True,
-                "internalType": "address",
-                "name": "previousOwner",
-                "type": "address",
-            },
-            {
-                "indexed": True,
-                "internalType": "address",
-                "name": "newOwner",
-                "type": "address",
-            },
-        ],
-        "name": "OwnershipTransferred",
-        "type": "event",
-    },
-    {
-        "anonymous": False,
-        "inputs": [
-            {
-                "indexed": True,
-                "internalType": "address",
-                "name": "from",
-                "type": "address",
-            },
-            {
-                "indexed": True,
-                "internalType": "address",
-                "name": "to",
-                "type": "address",
-            },
-            {
-                "indexed": True,
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256",
-            },
-        ],
-        "name": "Transfer",
-        "type": "event",
-    },
-    {
-        "inputs": [
-            {"internalType": "address", "name": "to", "type": "address"},
-            {"internalType": "uint256", "name": "tokenId", "type": "uint256"},
-        ],
-        "name": "approve",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function",
-    },
-    {
-        "inputs": [{"internalType": "address", "name": "owner", "type": "address"}],
-        "name": "balanceOf",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [{"internalType": "uint256", "name": "tokenId", "type": "uint256"}],
-        "name": "getApproved",
-        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [
-            {"internalType": "address", "name": "owner", "type": "address"},
-            {"internalType": "address", "name": "operator", "type": "address"},
-        ],
-        "name": "isApprovedForAll",
-        "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "name",
-        "outputs": [{"internalType": "string", "name": "", "type": "string"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "owner",
-        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [{"internalType": "uint256", "name": "tokenId", "type": "uint256"}],
-        "name": "ownerOf",
-        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "renounceOwnership",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function",
-    },
-    {
-        "inputs": [
-            {"internalType": "address", "name": "to", "type": "address"},
-            {"internalType": "string", "name": "tokenURI", "type": "string"},
-        ],
-        "name": "safeMint",
-        "outputs": [{"internalType": "uint256", "name": "tokenId", "type": "uint256"}],
-        "stateMutability": "nonpayable",
-        "type": "function",
-    },
-    {
-        "inputs": [
-            {"internalType": "address", "name": "from", "type": "address"},
-            {"internalType": "address", "name": "to", "type": "address"},
-            {"internalType": "uint256", "name": "tokenId", "type": "uint256"},
-        ],
-        "name": "safeTransferFrom",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function",
-    },
-    {
-        "inputs": [
-            {"internalType": "address", "name": "from", "type": "address"},
-            {"internalType": "address", "name": "to", "type": "address"},
-            {"internalType": "uint256", "name": "tokenId", "type": "uint256"},
-            {"internalType": "bytes", "name": "_data", "type": "bytes"},
-        ],
-        "name": "safeTransferFrom",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function",
-    },
-    {
-        "inputs": [
-            {"internalType": "address", "name": "operator", "type": "address"},
-            {"internalType": "bool", "name": "approved", "type": "bool"},
-        ],
-        "name": "setApprovalForAll",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function",
-    },
-    {
-        "inputs": [{"internalType": "bytes4", "name": "interfaceId", "type": "bytes4"}],
-        "name": "supportsInterface",
-        "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "symbol",
-        "outputs": [{"internalType": "string", "name": "", "type": "string"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [{"internalType": "uint256", "name": "index", "type": "uint256"}],
-        "name": "tokenByIndex",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [
-            {"internalType": "address", "name": "owner", "type": "address"},
-            {"internalType": "uint256", "name": "index", "type": "uint256"},
-        ],
-        "name": "tokenOfOwnerByIndex",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [{"internalType": "uint256", "name": "tokenId", "type": "uint256"}],
-        "name": "tokenURI",
-        "outputs": [{"internalType": "string", "name": "", "type": "string"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "totalSupply",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [
-            {"internalType": "address", "name": "from", "type": "address"},
-            {"internalType": "address", "name": "to", "type": "address"},
-            {"internalType": "uint256", "name": "tokenId", "type": "uint256"},
-        ],
-        "name": "transferFrom",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function",
-    },
-    {
-        "inputs": [{"internalType": "address", "name": "newOwner", "type": "address"}],
-        "name": "transferOwnership",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function",
-    },
-]
 contract = web3.eth.contract(address=ADDRESS, abi=ABI)
 
 # Start the Flask app
